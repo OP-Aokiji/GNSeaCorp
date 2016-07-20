0ba9392ce77a4f0f242a32445e3d13b854689440
OP-Aokiji <huynh.duc.khoa02@gmail.com>
2016-07-20 21:37:57 +0700

    2016-07-20 Admin Master page

---
+++ GNSeaCorp/Admin/MasterPage/GNSeaAdmin.Master.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace GNSeaCorp.Admin.MasterPage
+{
+    public partial class GNSeaAdmin : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
